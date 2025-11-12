144b4fee05d73fb03e8e8ae1fcd151d07294c07a
Maya <mayamartey@gmail.com>
2025-11-12 12:06:36 +0000

    Update app title from 'My sandwich shop' to 'My shop'

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '  My sandwich shop',
+      title: '  My shop',
       theme: ThemeData(
         // This is the theme of your application.
         //
